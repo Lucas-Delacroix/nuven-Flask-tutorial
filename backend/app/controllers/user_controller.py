d5ce6163900f7c4a230cde0dfae5fb0d676acdd4
Lucas-Delacroix <delacroixdev@gmail.com>
2024-04-08 18:10:11 -0300

    update 4.0

--- backend/app/controllers/user_controller.py
+++ backend/app/controllers/user_controller.py
@@ -32,6 +32,38 @@ def user_add():
         db.session.rollback()
         return jsonify({"error":True, "message": "Erro ao cadastrar usuário, informações já existem no banco."})
 
+@user.route("/user/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    """
+    Seção de tratamento de erro.
+    """
+    email_em_branco = "email" not in data or data["email"] is None or data["email"] == ""
+    senha_em_branco = "password" not in data or data["password"] is None or data["password"] == ""
+    data_none = data is None or data == "" or data == {}
+
+    if data_none:
+        return jsonify({"statusCode":"400", "name":"error", "message":"login falhou"}), "400"
+
+    if (email_em_branco) or (senha_em_branco):
+        return jsonify({"statusCode":"400", "name":"error", "message":"login falhou"}), "400"
+    
+    user = User.query.filter_by(email=data["email"]).first()
+    if user is None:
+        return jsonify({ "statusCode":"401", "name":"error", "message":"login falhou"}), "401"
+
+    if check_password_hash(user.password, data["password"]) == False:
+        return jsonify({ "statusCode":"401", "name":"error", "message":"login falhou"}), "401"
+    
+    created = datetime.now()
+    access_token = create_access_token(identity=user.email)
+    
+    #Decodificação do token JWT para obter o tempo de expiração (TLL), feito de acordo com as especificações e casos de teste.
+    decoded_token = decode_token(access_token)
+    token_ttl = decoded_token['exp'] - decoded_token['iat']
+
+    return jsonify({ "token": access_token, "ttl": token_ttl, "created": created, "userId": user.id})
+
 
 @user.route("/user/list", methods=["GET"])
 def user_list():
